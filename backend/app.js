8fec09b3de8a3b7a232f10fff3f4c5cb149e95e3
Shubham Mishra <shubhamishra583@gmail.com>
2023-04-18 22:36:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,12 @@ const cors = require('cors');
 //config
 dotenv.config({ path: "backend/config/config.env" });
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
